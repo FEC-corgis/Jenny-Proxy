08807f9651c00d550fd900b2a005a0b5cb10d5d4
Jenny Hou <jhou2012@gmail.com>
2021-01-30 15:59:51 -0500

    Connected first client-proxy-service using middleware.

--- server/index.js
+++ server/index.js
@@ -2,17 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.static('public'))
 
-
-app.get('/propId/:id', async (req, res) => {
-  axios(`http://localhost:1984${req.path}`)
-  .then(data => {
-    console.log('DATA', data)
-    res.send(data)
-  })
-})
+app.use('/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}.`)
