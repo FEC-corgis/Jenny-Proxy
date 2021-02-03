00a553fef67fc24f940483a4898c878ec42d7d42
Jenny Hou <jhou2012@gmail.com>
2021-02-03 14:43:17 -0500

    Added endpoints for classmates.

--- server/index.js
+++ server/index.js
@@ -6,8 +6,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.static('public'))
 
+// REVIEWS
 app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
 
+app.use('/reviews/overallRating/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
+
+app.use('/reviews/totalReviews/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
+
+// MOREPLACES
 app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
 
 app.listen(port, () => {
