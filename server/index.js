813802d34b7ace78b97a6e005caf6f2dd12cfeda
Jenny Hou <jhou2012@gmail.com>
2021-02-06 18:27:37 -0500

    Rendered Pauly service to proxy.

--- server/index.js
+++ server/index.js
@@ -6,11 +6,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use('/rooms/:id', express.static('public'))
 
+// JENNY
 app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
 
 app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
 
+// DANE
 app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }))
+
+// PAULY
+app.use('/propertyDetails/:pId', createProxyMiddleware({ target: 'http://localhost:5545', changeOrigin: true }))
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}.`)
