8c3766653cb44a4b72019172f2fc4df7bcad2fcf
Jenny Hou <jhou2012@gmail.com>
2021-02-06 16:23:47 -0500

    Fixed server endpoints to have one endpoint per service.

--- server/index.js
+++ server/index.js
@@ -6,15 +6,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use('/rooms/:id', express.static('public'))
 
-// REVIEWS
 app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
 
-app.use('/reviews/overallRating/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
-
-app.use('/reviews/totalReviews/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
-
-// MOREPLACES
 app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
+
+app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }))
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}.`)
