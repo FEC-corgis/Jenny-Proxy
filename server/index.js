37c656e6e83b785ef0eca9538879a3cf95128eb3
Jenny Hou <jhou2012@gmail.com>
2021-02-13 14:57:06 -0500

    s3 moreplaces test

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ app.use('/rooms/:id', express.static('public'))
 // app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
 app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://3.22.194.10:1984', changeOrigin: true }))
 
-app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
+// app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
+app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://18.222.249.216:1985', changeOrigin: true }))
 
 // DANE
 app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }))
