2270ecee71b116f04c65ca2fe9b30484f607b6db
Jenny Hou <jhou2012@gmail.com>
2021-02-15 01:41:47 -0500

    added google font weights

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const axios = require('axios')
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors')
 
 app.use('/rooms/:id', express.static('public'))
+app.use(cors())
 
 // JENNY
 // app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
@@ -14,10 +16,12 @@ app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://3.22.194.
 app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://18.222.249.216:1985', changeOrigin: true }))
 
 // DANE
-app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }))
+// app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }))
+app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://ec2-52-91-225-129.compute-1.amazonaws.com', changeOrigin: true }))
 
 // PAULY
-app.use('/propertyDetails/:pId', createProxyMiddleware({ target: 'http://localhost:5545', changeOrigin: true }))
+// app.use('/propertyDetails/:pId', createProxyMiddleware({ target: 'http://localhost:5545', changeOrigin: true }))
+app.use('/propertyDetails/:pId', createProxyMiddleware({ target: 'http://54.176.104.176:5545', changeOrigin: true }))
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}.`)
